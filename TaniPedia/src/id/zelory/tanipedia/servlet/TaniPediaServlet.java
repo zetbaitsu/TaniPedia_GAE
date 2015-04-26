d6587e0dc231e00904a7b2c68340e2af542a17ca
zetbaitsu <zetra@mail.ugm.ac.id>
2015-04-26 10:47:30 +0700

    Just a litle change.

--- TaniPedia/src/id/zelory/tanipedia/servlet/TaniPediaServlet.java
+++ TaniPedia/src/id/zelory/tanipedia/servlet/TaniPediaServlet.java
@@ -1,10 +1,14 @@
 package id.zelory.tanipedia.servlet;
+
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class TaniPediaServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+public class TaniPediaServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException
+	{
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
